b24c535e2fd3a3a2d63322e795008ef569d18bf5
Cristo Suarez <admync@gmail.com>
2019-08-01 08:52:26 +0200

    Add files via upload

---
+++ APIgestionhotel/src/main/java/com/cap/api/gestionhotel/GestionHotelApiApplication.java
@@ -0,0 +1,13 @@
+package com.cap.api.gestionhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionHotelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionHotelApiApplication.class, args);
+	}
+
+}
