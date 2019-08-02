6650d4ae74b5f73d5c6df65003c7abf8c5341161
Efren Rujas <uo225014@uniovi.es>
2019-08-02 09:43:03 +0200

    Commit productos y pago mapper

--- ApiGestionHotel/src/main/java/com/cap/apigestionhotel/dao/mapper/PagoMapper.java
+++ ApiGestionHotel/src/main/java/com/cap/apigestionhotel/dao/mapper/PagoMapper.java
@@ -2,11 +2,12 @@ package com.cap.apigestionhotel.dao.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import com.cap.apigestionhotel.dao.entity.Pago;
 
-
+@Mapper
 public interface PagoMapper {
 	
 	@Select("SELECT * FROM pago")
--- ApiGestionHotel/src/main/java/com/cap/apigestionhotel/dao/mapper/ProductosMapper.java
+++ ApiGestionHotel/src/main/java/com/cap/apigestionhotel/dao/mapper/ProductosMapper.java
@@ -2,11 +2,12 @@ package com.cap.apigestionhotel.dao.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import com.cap.apigestionhotel.dao.entity.Productos;
 
-
+@Mapper
 public interface ProductosMapper {
 	
 	@Select("SELECT * FROM productos")
