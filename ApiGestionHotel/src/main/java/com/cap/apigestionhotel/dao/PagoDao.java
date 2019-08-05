48c0e24336f4a21b51e71ff4f8777655ab8cb589
Crisuare <admync@gmail.com>
2019-08-05 13:47:01 +0200

    PagoDAO

--- ApiGestionHotel/src/main/java/com/cap/apigestionhotel/dao/PagoDao.java
+++ ApiGestionHotel/src/main/java/com/cap/apigestionhotel/dao/PagoDao.java
@@ -1,5 +1,6 @@
 package com.cap.apigestionhotel.dao;
 
+import java.sql.Date;
 import java.util.List;
 
 import com.cap.apigestionhotel.dao.entity.Pago;
@@ -7,5 +8,9 @@ import com.cap.apigestionhotel.dao.entity.Pago;
 public interface PagoDao {
 	
 	public List<Pago> findAll();
+	public Pago findPago(int pag_id);
+	public void insert(Pago pago);
+	public void update(Pago pago);
+	public void delete(int pag_id);
 
 }
