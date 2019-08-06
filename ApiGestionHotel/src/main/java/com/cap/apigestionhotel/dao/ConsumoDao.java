22ee40e23f9e4a4263faa1e2c28f44830a6909b8
Efren Rujas <uo225014@uniovi.es>
2019-08-06 08:26:49 +0200

    Commit ConsumoDao

--- ApiGestionHotel/src/main/java/com/cap/apigestionhotel/dao/ConsumoDao.java
+++ ApiGestionHotel/src/main/java/com/cap/apigestionhotel/dao/ConsumoDao.java
@@ -3,9 +3,15 @@ package com.cap.apigestionhotel.dao;
 import java.util.List;
 
 import com.cap.apigestionhotel.dao.entity.Consumo;
+import com.cap.apigestionhotel.dao.entity.Consumo;
 
 public interface ConsumoDao {
 
 	public List<Consumo> findAll();
+	
+	public Consumo findConsumo(int con_id);
+	public void insert(Consumo consumo);
+	public void update(Consumo consumo);
+	public void delete(int con_id);
 	
 }
