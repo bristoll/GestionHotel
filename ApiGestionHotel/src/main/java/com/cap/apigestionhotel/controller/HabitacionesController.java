c0e037f8966b5704da0ff129cb74a67318ec4a03
Mariova15 <mariovigilescalera@gmail.com>
2019-08-06 09:22:59 +0200

    HabitacionesController

--- ApiGestionHotel/src/main/java/com/cap/apigestionhotel/controller/HabitacionesController.java
+++ ApiGestionHotel/src/main/java/com/cap/apigestionhotel/controller/HabitacionesController.java
@@ -5,10 +5,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cap.apigestionhotel.dao.entity.Clientes;
 import com.cap.apigestionhotel.dao.entity.Habitaciones;
 import com.cap.apigestionhotel.impl.HabitacionesImpl;
 
@@ -24,4 +30,26 @@ public class HabitacionesController {
 		return new ResponseEntity<>(habitacionesImpl.findAll(), HttpStatus.OK);
     }
 	
+	@GetMapping("/clientes/{cli_dni}")
+    public ResponseEntity<Habitaciones> findCliente(@PathVariable int ha_id) {
+		return new ResponseEntity<>(habitacionesImpl.findHabitaciones(ha_id), HttpStatus.OK);
+    }
+	
+	@PostMapping("/clientes")
+    public ResponseEntity<Clientes> insert(@ModelAttribute Habitaciones habitacione) {        
+		habitacionesImpl.insert(habitacione);
+        return new ResponseEntity<Clientes>(HttpStatus.CREATED);
+    }
+	
+	@PutMapping("/clientes/{idClientes}")
+	public ResponseEntity<Clientes> update(@ModelAttribute Habitaciones habitacione) {
+		habitacionesImpl.update(habitacione);
+		return new ResponseEntity<Clientes>(HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/clientes/{idClientes}")
+	public ResponseEntity<Clientes> delete(@ModelAttribute Habitaciones habitacione) {
+		habitacionesImpl.delete(habitacione.getHa_id());
+		return new ResponseEntity<Clientes>(HttpStatus.OK);
+	}
 }
