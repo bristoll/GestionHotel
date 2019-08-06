87f0a7ff656ea2afe1669b86e5a4ef52ef56ecc6
Crisuare <admync@gmail.com>
2019-08-06 08:37:34 +0200

    Mappers hoteles,reservas

--- ApiGestionHotel/src/main/java/com/cap/apigestionhotel/dao/mapper/ReservasMapper.java
+++ ApiGestionHotel/src/main/java/com/cap/apigestionhotel/dao/mapper/ReservasMapper.java
@@ -2,19 +2,32 @@ package com.cap.apigestionhotel.dao.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.cap.apigestionhotel.dao.entity.Reservas;
+
+
 
 
 @Mapper
 public interface ReservasMapper {
 
-	@Select("SELECT * FROM clientes")
+	@Select("SELECT * FROM reservas")
 	List<Reservas> findAll();
 	
-	@Select("SELECT * FROM clientes where re_id = #{re_id}")
+	@Select("SELECT * FROM reservas where re_id = #{re_id}")
 	Reservas findReserva(String re_id);
 
+	@Insert("INSERT INTO reservas (re_id, re_cli_dni, re_ha_id, re_fecha_reserva, re_fecha_ini,re_fecha_fin,re_coste_alojamiento,re_estado) "
+			+ "VALUES (#{re_id}, #{re_cli_dni}, #{re_ha_id}, #{re_fecha_reserva}, #{re_fecha_ini},#{re_fecha_fin},#{re_coste_alojamiento},#{re_estado}")
+    void insert(Reservas reserva);
+	
+	@Update("UPDATE reservas SET re_cli_dni = #{re_cli_dni}, re_ha_id = #{re_ha_id}, re_fecha_reserva = #{re_fecha_reserva}, re_fecha_ini = #{re_fecha_ini}, re_fecha_fin = #{re_fecha_fin}, re_coste_alojamiento = #{re_coste_alojamiento}, re_estado = #{re_estado} where re_id=#{re_id}")
+	void update(Reservas reserva);
+	
+	@Update("DELETE from reservas where re_id=#{re_id}")
+	void delete(int re_id);
 }
