8f1fce0bf06eefdcb3ac372a3a665adfdaefbd69
Efren Rujas <uo225014@uniovi.es>
2019-08-07 09:26:29 +0200

    Commit ProductosService

---
+++ ApiGestionHotel/src/main/java/com/cap/apigestionhotel/service/ProductosService.java
@@ -0,0 +1,56 @@
+package com.cap.apigestionhotel.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.cap.apigestionhotel.dao.entity.Clientes;
+import com.cap.apigestionhotel.dao.entity.Productos;
+import com.cap.apigestionhotel.dto.ClienteSimpleDto;
+import com.cap.apigestionhotel.impl.ProductosImpl;
+
+@Service
+public class ProductosService {
+
+	@Autowired
+	ProductosImpl productosImpl;
+	
+	public ResponseEntity<List<Productos>> findAll() {
+		return new ResponseEntity<>(productosImpl.findAll(), HttpStatus.OK);
+    }
+	
+    public ResponseEntity<Productos> findProductos(int pro_id) {
+		return new ResponseEntity<>(productosImpl.findProductos(pro_id), HttpStatus.OK);
+    }
+    
+    public ResponseEntity<Productos> insert(Productos producto) {        
+        productosImpl.insert(producto);
+        return new ResponseEntity<Productos>(HttpStatus.CREATED);//La cadena es opcional
+    }
+    
+	public ResponseEntity<Productos> update(Productos producto) {//No se si es correcto meterle el nombre aqui ya que no aparece en la uri
+		productosImpl.update(producto);
+		return new ResponseEntity<Productos>(HttpStatus.OK);
+	}
+	
+	public ResponseEntity<Productos> delete(int pro_id) {
+		productosImpl.delete(pro_id);
+		return new ResponseEntity<Productos>(HttpStatus.OK);
+	}
+	
+
+	
+//    public ResponseEntity<ClienteSimpleDto> findClienteSimple(String cli_dni) {
+//		
+//		Productos cliente = productosImpl.findProductos(cli_dni);
+//		String nombreCompleto = cliente.getCli_nombre() + " " + cliente.getCli_apellido();
+//		
+//		return new ResponseEntity<>(new ClienteSimpleDto(cliente.getCli_dni(), nombreCompleto, cliente.getCli_email(), cliente.getCli_ciudad()), HttpStatus.OK);
+//    }
+	
+}
