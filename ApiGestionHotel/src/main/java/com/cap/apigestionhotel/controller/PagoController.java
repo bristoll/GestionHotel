ceb7f53a83c17e06a0ba2c27db928c0a14c39ffa
Efren Rujas <uo225014@uniovi.es>
2019-08-07 10:50:31 +0200

    Commit cambios controllers de pagos y productos

--- ApiGestionHotel/src/main/java/com/cap/apigestionhotel/controller/PagoController.java
+++ ApiGestionHotel/src/main/java/com/cap/apigestionhotel/controller/PagoController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,7 +44,7 @@ public class PagoController {
 		
 		
 		@PostMapping("/pago")
-	    public ResponseEntity<Pago> insert(@ModelAttribute Pago pago) {
+	    public ResponseEntity<Pago> insert(@RequestBody Pago pago) {
 			pagoService.insert(pago);
 	        return new ResponseEntity<Pago>(HttpStatus.CREATED);//La cadena es opcional
 	    }
