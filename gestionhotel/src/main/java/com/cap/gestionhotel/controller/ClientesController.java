2e51bb2d4571f757a6f963cfcca6df17a847cd78
Mariova15 <mariovigilescalera@gmail.com>
2019-08-09 13:27:24 +0200

    ControllerServiceLogin

--- gestionhotel/src/main/java/com/cap/gestionhotel/controller/ClientesController.java
+++ gestionhotel/src/main/java/com/cap/gestionhotel/controller/ClientesController.java
@@ -3,13 +3,19 @@ package com.cap.gestionhotel.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.cap.gestionhotel.model.ClienteLoginDto;
+import com.cap.gestionhotel.model.ClienteSimpleDto;
 import com.cap.gestionhotel.service.ClientesService;
 
 
@@ -24,6 +30,21 @@ public class ClientesController {
 	public String listaClientes(Model model) {
 		model.addAttribute("listaClientes", clientesService.listaClientes());
 		return "listaClientes";
+	}
+	
+	@GetMapping("/login")
+	public ModelAndView login(ModelAndView modelAndView, @ModelAttribute("clienteLogin") @Validated ClienteLoginDto clienteLoginDto) {
+
+		ResponseEntity<ClienteSimpleDto> response = clientesService.login(clienteLoginDto);
+		
+		if (response.getStatusCode().equals(HttpStatus.OK)) {
+			modelAndView.addObject("clienteLogin", response.getBody());
+			modelAndView.setViewName("listaClientes");
+		}else {
+			modelAndView.setViewName("index");
+		}
+
+		return modelAndView;
 	}
 	
 }
---
+++ gestionhotel/src/main/java/com/cap/gestionhotel/model/ClienteSimpleDto.java
@@ -0,0 +1,43 @@
+package com.cap.gestionhotel.model;
+
+public class ClienteSimpleDto {
+
+	private String dni;
+	private String nombreCompleto;
+	private String email;
+	private String ciudad;
+	
+	public ClienteSimpleDto(String dni, String nombreCompleto, String email, String ciudad) {
+		this.dni = dni;
+		this.nombreCompleto = nombreCompleto;
+		this.email = email;
+		this.ciudad = ciudad;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getNombreCompleto() {
+		return nombreCompleto;
+	}
+	public void setNombreCompleto(String nombreCompleto) {
+		this.nombreCompleto = nombreCompleto;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCiudad() {
+		return ciudad;
+	}
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+	
+	
+	
+}
