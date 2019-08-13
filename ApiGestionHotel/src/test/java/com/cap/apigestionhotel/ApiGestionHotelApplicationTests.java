2d5ec604c63d6c97bcd79676c9713da05864dc04
Mariova15 <mariovigilescalera@gmail.com>
2019-08-13 09:10:25 +0200

    Revert "Merge branch 'Desarrollo' into DesarrolloMario"
    This reverts commit 63fcffb2ff6e0fada5b8836de0029464cc67fc52, reversing
    changes made to 2b5c3bedd56fd3605541c5319431a1b74a3628a9.

--- ApiGestionHotel/src/test/java/com/cap/apigestionhotel/ApiGestionHotelApplicationTests.java
+++ ApiGestionHotel/src/test/java/com/cap/apigestionhotel/ApiGestionHotelApplicationTests.java
@@ -1,6 +1,5 @@
 package com.cap.apigestionhotel;
 
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
--- gestionhotel/src/main/java/com/cap/gestionhotel/controller/AppController.java
+++ gestionhotel/src/main/java/com/cap/gestionhotel/controller/AppController.java
@@ -11,16 +11,18 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class AppController {
 
-	@GetMapping(value = "/")
-	public ModelAndView index(ModelAndView modelAndView) {
-		modelAndView.setViewName("index");
-		return modelAndView;
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView defaultIndex() {
+		ModelAndView model = new ModelAndView();
+		model.setViewName("index");
+		return model;
 	}
 		
-	@GetMapping(value = "/login")
-	public ModelAndView login(ModelAndView modelAndView) {
-		modelAndView.setViewName("login");
-		return modelAndView;
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView login() {
+		ModelAndView model = new ModelAndView();
+		model.setViewName("login");
+		return model;
 	}
 	
 	@RequestMapping(value = "/add", method = RequestMethod.GET)
@@ -28,26 +30,5 @@ public class AppController {
 		ModelAndView model = new ModelAndView();
 		model.setViewName("addcliente");
 		return model;
-	}
-}
-package com.cap.gestionhotel.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-@Controller
-public class AppController {
-
-	@GetMapping(value = "/")
-	public ModelAndView index(ModelAndView modelAndView) {
-		modelAndView.setViewName("index");
-		return modelAndView;
-	}
-	
-	@GetMapping(value = "/login")
-	public ModelAndView login(ModelAndView modelAndView) {
-		modelAndView.setViewName("login");
-		return modelAndView;
 	}
 }
