ffac48a9227069b5398d61c9594cd77123b7ec00
Crisuare <admync@gmail.com>
2019-08-13 08:22:12 +0200

    Cambios para no hacer new de modelandview

--- gestionhotel/src/main/java/com/cap/gestionhotel/controller/AppController.java
+++ gestionhotel/src/main/java/com/cap/gestionhotel/controller/AppController.java
@@ -1,27 +1,21 @@
 package com.cap.gestionhotel.controller;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AppController {
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView index() {
-		ModelAndView model = new ModelAndView();
-		model.setViewName("index");
-		return model;
+	@GetMapping(value = "/")
+	public ModelAndView index(ModelAndView modelAndView) {
+		modelAndView.setViewName("index");
+		return modelAndView;
 	}
 	
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public ModelAndView login() {
-		ModelAndView model = new ModelAndView();
-		model.setViewName("login");
-		return model;
+	@GetMapping(value = "/login")
+	public ModelAndView login(ModelAndView modelAndView) {
+		modelAndView.setViewName("login");
+		return modelAndView;
 	}
 }
