cb1f345e196f7343f57ea568fcdba5d3ae2c4f2f
Crisuare <admync@gmail.com>
2019-08-21 10:45:32 +0200

    commit ajax

--- gestionhotel/src/main/webapp/content/static/filtro.js
+++ gestionhotel/src/main/webapp/content/static/filtro.js
@@ -14,19 +14,49 @@ var btn = document.getElementById("btn");
 //
 //}
 
+//function pruebaAjax(){
+//	
+//		  var listaLibres;
+//		  var xhttp = new XMLHttpRequest();
+//		  xhttp.onreadystatechange = function() {
+//		    if (this.readyState == 4 && this.status == 200) {
+//		      document.getElementById("demo").innerHTML = "ajax ejecutado correctamente";
+//		      listaLibres=this.response;
+//		      //console.log(listaLibres);
+////		      var o = { "key1": [1,2,3], "key2": "value2"};
+////		      for(var prop in o) {
+////		        console.log(prop,o[prop]);  
+////		      }
+//		    var lista = JSON.parse(listaLibres);
+//		    lista.forEach(function(element) {
+//		    	  console.log(element);
+//		    	}); 
+//		  };
+//		  xhttp.open("GET", "/clientes/listaLibres/3", true);
+//		  //xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+//		  xhttp.send();
+//		  }
+//}
 function pruebaAjax(){
 	
-		  var listaLibres;
-		  var xhttp = new XMLHttpRequest();
-		  xhttp.onreadystatechange = function() {
-		    if (this.readyState == 4 && this.status == 200) {
-		      document.getElementById("demo").innerHTML = "ajax ejecutado correctamente";
-		      listaLibres=this.response;
-		      console.log(listaLibres);
-		    }
-		  };
-		  xhttp.open("GET", "/clientes/listaLibres/3", true);
-		  //xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		  xhttp.send();
-		}
-	
+	  var listaLibres;
+	  var xhttp = new XMLHttpRequest();
+	  xhttp.onreadystatechange = function() {
+	    if (this.readyState == 4 && this.status == 200) {
+	      document.getElementById("demo").innerHTML = "ajax ejecutado correctamente";
+	      listaLibres=this.response;
+	      console.log(listaLibres);
+	      var lista = JSON.parse(listaLibres);
+	      console.log(lista);
+	      
+	      for(var o in lista){
+	    	  
+	    	  console.log(o);
+	    	  
+	      }
+	    }
+	  };
+	  xhttp.open("GET", "/clientes/listaLibres/3", true);
+	  //xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	  xhttp.send();
+	}
